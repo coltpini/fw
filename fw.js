a8ddc29ad0a53c49535fa0f7de3d0459eb71c60e
coltpini <coltpini@gmail.com>
2013-01-29 14:20:15 -0700

    Added extend
    Added mechanism to extend both fw and fwElement. Also removed Add
    loading and made it an extention.

--- fw.js
+++ fw.js
@@ -458,6 +458,13 @@ fw.dtoIE = function(deg) {
 	return 'progid:DXImageTransform.Microsoft.Matrix(M11=' + cos + ', M12=' + -sin + ', M21=' + sin + ', M22=' + cos + ",SizingMethod='auto expand')";
 };
 
+fw.extendElement = function(name,func){
+	this.extendObject[name] = func;
+	Element.prototype[name] = func;
+};
+
+fw.extendObject = {};
+
 var FW = function(fwObj){
 	if(fwObj.isFW){ return fwObj;}
 	fwObj.isFW = true;
@@ -600,69 +607,6 @@ var FW = function(fwObj){
 		return this;
 	};
 
-	fwObj.addLoading = function(seg, opt, style, loadingFunction){
-		this.removeLoading();
-		if(this.style.position === "relative" || this.style.position === "absolute"){}
-		else
-			this.style.position = 'relative';
-
-		var loading = this.createChild('<div>').addClass('loading');
-		if(typeof(style) === "string")loading.addClass(style);
-
-		var div = loading.createChild("div");
-
-		var	transformSupport = fw.styleProp("transform"),
-			animationSupport = fw.styleProp("animation");
-
-		if(!transformSupport.is || !animationSupport.is){
-			div.addClass('fallback');
-		}
-		else{
-			var segments = seg || 8,
-				degrees = Math.round(360 / segments);
-
-			for(var i=1; i<segments+1; i++){
-
-				var d = div.createChild("div").addClass('segment'),
-					span = d.createChild("span"),
-					deg = (degrees * i) + "deg",
-					num=Math.random()*3,
-					hi=30,
-					wi=8,
-					opac = 2 / i;
-
-				d.style[transformSupport.prop] = "rotate(" + deg + ")";
-				d.style[animationSupport.prop] = "loader " + num + "s linear infinite";
-
-				if(loadingFunction){
-					loadingFunction(segments,opt,i,d,span);
-				}
-				else {
-					if(opt === "tornado"){
-						wi = num * i * i;
-					}
-					else if(opt === "traditional"){
-						d.style[animationSupport.prop] = "none";
-						span.style.borderRadius = "2px";
-						wi = 2 * Math.PI * 8;
-						opac = 10 / (i+1);
-					}
-					else if(opt > 0){
-						wi = opt;
-					}
-
-					hi = wi * Math.PI / seg;
-					d.style.width = wi + "px";
-					d.style.height = hi + "px";
-					d.style.marginLeft = -(wi/2) + "px";
-					d.style.marginTop = -(hi/2) + "px";
-					d.opacity(opac);
-				}
-			}
-		}
-	};
-
-
 	fwObj.opacity = function(num){
 		if(this.style && typeof(this.style.opacity) === "undefined")
 			this.style.filter = "alpha(opacity:" + num * 100 + ")";
@@ -670,13 +614,10 @@ var FW = function(fwObj){
 			this.style.opacity = num;
 	};
 
-	fwObj.removeLoading = function(){
-		var l = this.find(".loading");
-		if(l){
-			l.opacity = 0;
-			setTimeout(function(){l.parentNode.removeChild(l);},100);
-		}
-	};
+	for(var prop in fw.extendObject){
+		fwObj[prop] = fw.extendObject[prop];
+	}
+
 	return fwObj;
 };
 FW(Element.prototype);
---
+++ fwLoading.js
@@ -0,0 +1,68 @@
+fw.extendElement("addLoading", function(seg, opt, style, loadingFunction){
+	this.removeLoading();
+	if(this.style.position === "relative" || this.style.position === "absolute"){}
+	else
+		this.style.position = 'relative';
+
+	var loading = this.createChild('<div>').addClass('loading');
+	if(typeof(style) === "string")loading.addClass(style);
+
+	var div = loading.createChild("div");
+
+	var	transformSupport = fw.styleProp("transform"),
+		animationSupport = fw.styleProp("animation");
+
+	if(!transformSupport.is || !animationSupport.is){
+		div.addClass('fallback');
+	}
+	else{
+		var segments = seg || 8,
+			degrees = Math.round(360 / segments);
+
+		for(var i=1; i<segments+1; i++){
+
+			var d = div.createChild("div").addClass('segment'),
+				span = d.createChild("span"),
+				deg = (degrees * i) + "deg",
+				num=Math.random()*3,
+				hi=30,
+				wi=8,
+				opac = 2 / i;
+
+			d.style[transformSupport.prop] = "rotate(" + deg + ")";
+			d.style[animationSupport.prop] = "loader " + num + "s linear infinite";
+
+			if(loadingFunction){
+				loadingFunction(segments,opt,i,d,span);
+			}
+			else {
+				if(opt === "tornado"){
+					wi = num * i * i;
+				}
+				else if(opt === "traditional"){
+					d.style[animationSupport.prop] = "none";
+					span.style.borderRadius = "2px";
+					wi = 2 * Math.PI * 8;
+					opac = 10 / (i+1);
+				}
+				else if(opt > 0){
+					wi = opt;
+				}
+
+				hi = wi * Math.PI / seg;
+				d.style.width = wi + "px";
+				d.style.height = hi + "px";
+				d.style.marginLeft = -(wi/2) + "px";
+				d.style.marginTop = -(hi/2) + "px";
+				d.opacity(opac);
+			}
+		}
+	}
+});
+fw.extendElement("removeLoading",function(){
+	var l = this.find(".loading");
+	if(l){
+		l.opacity = 0;
+		setTimeout(function(){l.parentNode.removeChild(l);},100);
+	}
+});
