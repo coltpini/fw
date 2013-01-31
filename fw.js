31f1a63214564aeb0c69014bf714808dd6885cdd
coltpini <coltpini@gmail.com>
2013-01-30 17:43:31 -0700

    removed xml and json and moved to extension

--- fw.js
+++ fw.js
@@ -267,68 +267,6 @@ fw.jsonp = function(options){
 	fw.loadScript(o.url + '?callback=' + key);
 };
 
-fw.stringToXml = function(str){
-	var xml;
-	str = str.replace(/[\t\n]/gi,"");
-	if (typeof window.DOMParser !== "undefined") {
-		xml = (new window.DOMParser()).parseFromString(str, "text/xml");
-	}
-	else if (typeof window.ActiveXObject !== "undefined" && new window.ActiveXObject("Microsoft.XMLDOM")) {
-		xml = new window.ActiveXObject("Microsoft.XMLDOM");
-		xml.async = "false";
-		xml.loadXML(str);
-	}
-	else
-		xml = undefined;
-
-	return xml;
-};
-
-// Changes XML to JSON
-fw.xmlToJson = function(rxml) {
-	var obj = {},
-		xml = rxml;
-	if(typeof(rxml) === "string"){
-		xml = this.stringToXml(rxml);
-		if(!xml)
-			return obj;
-	}
-
-	if (xml.nodeType === 1) { // element
-		// do attributes
-		if (xml.attributes.length > 0) {
-			obj["@attributes"] = {};
-			for (var j = 0; j < xml.attributes.length; j++) {
-				var attribute = xml.attributes.item(j);
-				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-			}
-		}
-	}
-	else if (xml.nodeType === 3) { // text
-		obj = xml.nodeValue;
-	}
-
-	// do children
-	if (xml.hasChildNodes()) {
-		for(var i = 0; i < xml.childNodes.length; i++) {
-			var item = xml.childNodes.item(i);
-			var nodeName = item.nodeName;
-
-			if (typeof(obj[nodeName]) === "undefined") {
-				obj[nodeName] = this.xmlToJson(item);
-			}
-			if (typeof(obj[nodeName].push) === "undefined") {
-				var old = obj[nodeName];
-				obj[nodeName] = [];
-				obj[nodeName].push(old);
-			}
-			else
-				obj[nodeName].push(this.xmlToJson(item));
-		}
-	}
-	return obj;
-};
-
 fw.randomString = function(length, isAlphaNumeric){
 	var s= '';
 	var randomchar=function(){
